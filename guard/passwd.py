1b3bbe5f098353cf358d298594d03d326998ac24
Changlong <ncaewk@gmail.com>
2017-03-23 18:02:43 +0800

    Update password manager.

--- guard/passwd.py
+++ guard/passwd.py
@@ -6,7 +6,8 @@ import os
 class PwManager(object):
     @staticmethod
     def get_passwd_hash():
-        file_path = os.path.dirname(os.getcwd()) + '/config.ini'
+        # file_path = os.path.dirname(os.getcwd()) + '/config.ini'
+        file_path = os.getcwd() + '/config.ini'
 
         if os.path.exists(file_path):
             # print file_path
@@ -18,13 +19,20 @@ class PwManager(object):
             return m.hexdigest()
 
         else:
-            PwManager.update_passwd('e10adc3949ba59abbe56e057f20f883e', '123456')
+            file_path = os.path.dirname(os.getcwd()) + '/config.ini'
+            ini_file = open(file_path, 'w')
+            ini_config = ConfigParser.ConfigParser()
+            ini_config.add_section('User')
+            ini_config.set('User', 'psw', '123456')
+            ini_config.write(ini_file)
+            ini_file.close()
             return 'e10adc3949ba59abbe56e057f20f883e'
 
     @staticmethod
     def update_passwd(old_psw_md5, new_psw):
         if old_psw_md5 == PwManager.get_passwd_hash():
-            file_path = os.path.dirname(os.getcwd()) + '/config.ini'
+            # file_path = os.path.dirname(os.getcwd()) + '/config.ini'
+            file_path = os.getcwd() + '/config.ini'
             ini_file = open(file_path, 'w')
             ini_config = ConfigParser.ConfigParser()
             ini_config.add_section('User')
